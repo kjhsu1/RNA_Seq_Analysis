ae27250af2170feb95d233e04e4691a0bf22e962
yusuf786ali <yusuf876ali@gmail.com>
2025-03-02 15:38:56 -0800

    update

---
+++ Yusuf/intronprobreads.py
@@ -0,0 +1,25 @@
+import sys
+import gzip
+
+filepath = sys.argv[1]
+
+def counttotal():
+    total_reads = 0
+    with gzip.open(filepath, 'rt') as fp:
+        for line in fp:
+            cols = line.split()
+            if cols[1] == 'RNASeq_splice' and cols[2] == 'intron':
+                total_reads += int(cols[5])
+        return total_reads
+print(counttotal())
+
+def prob_intron(start, end, strand): 
+    with gzip.open(filepath, 'rt') as fp:
+        for line in fp:
+            cols = line.split()
+            if int(cols[3]) == start and int(cols[4]) == end and cols[6] == strand:
+                seqprob = int(cols[5]) / counttotal()
+        return seqprob
+                
+            
+print(prob_intron(3258, 3504, '-'))
